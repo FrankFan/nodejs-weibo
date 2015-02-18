1f081f7bb34357774cb1544ebbb29b9898642e9d
FrankFan <fanyong@gmail.com>
2015-02-18 11:43:36 +0800

    use express-paritials to do layout

--- microblog/app.js
+++ microblog/app.js
@@ -4,15 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// express 3.0+ 取消了对默认layout.ejs的支持，必须手动引用express-partials
+var partials = require('express-partials'); 
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var hello = require('./routes/hello');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(partials()); // 这里必须要use
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -24,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/hello', hello);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -59,4 +64,4 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-console.log('running at port 3000');
+console.log("Express server listening on port %d in %s mode \n", 3000, app.settings.env);
--- microblog/routes/users.js
+++ microblog/routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.render('users', { title: 'users', content: 'respond with a resource', layout: 'layout' });
 });
 
 module.exports = router;
